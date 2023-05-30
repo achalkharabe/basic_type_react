9577df9d19cb8a4793667634faf51b93fb057767
achalkharabe <achalkharabe1@gmail.com>
2023-05-30 15:11:26 +0530

    commit all

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BaseComponent from './BaseComponent/BaseComponent';
+
+function App() {
+  return (
+   <div>
+    <BaseComponent title='arc'   name='achal kharabe'></BaseComponent>
+    
+    <p>hellow achal</p>
+   </div>
+  );
+}
+
+export default App;
